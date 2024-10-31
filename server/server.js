8f46a250cc931bccc2d8783f8a967a7574aac894
redbot <redbot@cyberservices.com>
2024-10-30 22:48:02 -0400

    update

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodaX!'
+    message: 'Hello from CodeX!'
   })
 })
 
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(443, () => console.log('AI server started on https://codex-xgik.onrender.com'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
